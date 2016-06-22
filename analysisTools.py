aa493783fa981987486ca0eed67fbc4fba3d581b
Kevin Nelson <Kevin Nelson>
2016-06-22 13:26:54 -0400

    haste makes waste

--- analysisTools.py
+++ analysisTools.py
@@ -11,6 +11,6 @@ def BiSec(v, method = "mean"):
 		avg = sum(v)/float(len(v))
 	if(method == "median"):
 		avg = sum(sorted(v)[len(v)/2-1:len(v)/2+1 if len(v)%2 == 0 else len(v)/2:len(v)/2+1])
-	return([1 if k>mode else 0 for k in v])
+	return([1 if k>avg else 0 for k in v])
 
 
